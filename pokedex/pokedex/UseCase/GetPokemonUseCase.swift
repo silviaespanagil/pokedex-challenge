a56a10d4e7286c2d4bcf4163386b4b8aa9fb3ba8
Silvia España <silviaespanagil@gmail.com>
2022-05-04 16:54:22 +0200

    Add new usecase for get pokemon

---
+++ pokedex/pokedex/UseCase/GetPokemonUseCase.swift
@@ -0,0 +1,24 @@
+//
+//  GetPokemonUseCase.swift
+//  pokedex
+//
+//  Created by Silvia España on 4/5/22.
+//
+
+import Foundation
+import Combine
+
+class GetPokemonUseCase {
+    
+    private let repository: PokemonRepository
+    
+    init(repository: PokemonRepository = PokemonRepositoryImplementation()) {
+
+        self.repository = repository
+    }
+
+    func execute(id: Int) -> AnyPublisher<Pokemon, Error> {
+
+        repository.getPokemon(id: id)
+    }
+}
