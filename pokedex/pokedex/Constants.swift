3671aa343bfc77e609976892c9c707aa3ddc8366
Silvia España <silviaespanagil@gmail.com>
2022-05-04 15:54:19 +0200

    Create APIManager

---
+++ pokedex/pokedex/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Silvia España on 4/5/22.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let baseURL =  "https://pokeapi.co/api/v2/"
+}
