13e9d43b3d7eee996e44da65965c7a3936199921
Silvia España <silviaespanagil@gmail.com>
2022-05-04 15:30:24 +0200

    Setup project -> First commit

---
+++ pokedex/pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Silvia España on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
