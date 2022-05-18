cacd627996e6521e34fe9fb35cafe21aa40b5825
Silvia España <silviaespanagil@gmail.com>
2022-05-18 16:46:24 +0200

    Update ButtonView to take width as optional parameter

--- pokedex/pokedex/Views/Buttons/ButtonView.swift
+++ pokedex/pokedex/Views/Buttons/ButtonView.swift
@@ -12,12 +12,14 @@ struct ButtonView: View {
     var action: () -> Void
     var image: String
     var helpText: String
+    var width: CGFloat?
     
-    init(action: @escaping () -> Void, image: String, helpText: String) {
+    init(action: @escaping () -> Void, image: String, helpText: String, width: CGFloat? = 120) {
         
         self.action = action
         self.image = image
         self.helpText = helpText
+        self.width = width
     }
     
     var body: some View {
@@ -30,7 +32,7 @@ struct ButtonView: View {
             Image("")
                 .renderImage(url: URL(string: image)!)
                 .help(helpText)
-                .frame(width: 100, height: 100)
+                .frame(width: width, height: 120)
                 .padding()
         }
     }
