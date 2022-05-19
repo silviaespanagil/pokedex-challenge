c3fa7c70dede7953bf8ad5274926310ed28cfa44
Silvia España <silviaespanagil@gmail.com>
2022-05-19 15:11:00 +0200

    Update view to take new images and change copy

--- pokedex/pokedex/Scenes/BackpackView/EmptyBackpack/EmptyBackpackView.swift
+++ pokedex/pokedex/Scenes/BackpackView/EmptyBackpack/EmptyBackpackView.swift
@@ -18,22 +18,19 @@ struct EmptyBackpackView: View {
                 Text("Looks like your backpack is empty :(")
                     .font(.title)
                 
-                Image("")
-                    .renderImage(url: URL(string: "https://media0.giphy.com/media/Wj381DH2BLXF9FP3ez/giphy.gif?cid=ecf05e47cfj44zwuz3gbu501gdc9l41935rzr6h3kmcwc7va&rid=giphy.gif&ct=s")!)
-                    .frame(width: 50, height: 50)
+                Image("Backpack")
                     .padding()
                     .rotationEffect(.degrees(-22))
             }.padding(.bottom)
             
-            Text("Click on your Gameboy to find your first Pokemon")
+            Text("You better travel to the Pokeworld and catch them all!")
                 .padding(.bottom)
             
             Text("\"Some trainers have no fear. To them, this is just one more challenge.\"")
                 .font(.footnote)
                 .foregroundColor(.gray)
             
-        }
-            .padding()
+        }.padding()
     }
 }
 
