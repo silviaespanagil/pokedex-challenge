1a48ff56bba3957574cb43b7008909c03eceb6a0
Silvia España <silviaespanagil@gmail.com>
2022-06-01 14:21:15 +0200

    Refactored Pokeworld to render by states

---
+++ pokedex/pokedex/Scenes/PokeworldView/PokeworldViewState.swift
@@ -0,0 +1,18 @@
+//
+//  PokeworldViewState.swift
+//  pokedex
+//
+//  Created by Silvia España on 1/6/22.
+//
+
+import Foundation
+
+import Foundation
+
+enum PokemonWorldViewState: Equatable {
+    
+    case empty
+    case loading
+    case loaded
+    case failed
+}
